be84a43bab6b580f30fb8426d8e7283a1d48464f
mateja <mateja176@gmail.com>
2019-05-07 15:50:34 +0200

    fix: tests

--- src/App.toBeInDocument.test.tsx
+++ src/App.toBeInDocument.test.tsx
@@ -4,5 +4,5 @@ import App from './App';
 
 it('renders welcome message', () => {
   const { getByText } = render(<App />);
-  expect(getByText('src/App.tsx?')).toBeInTheDocument();
+  expect(getByText('src/App.tsx')).toBeInTheDocument();
 });
